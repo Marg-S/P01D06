381fe2e7f9a612019f0c8c8fbb7cd56f1fcea001
Vados Hortensia <vadoshor@ge-h3.kzn.21-school.ru>
2023-01-29 12:56:27 +0300

    v3.0

--- src/pong.c
+++ src/pong.c
@@ -1,116 +1,131 @@
-#include <math.h>
 #include <stdio.h>
 
-int Racketka_1 = 12;
-int Racketka_2 = 12;
+int width = 80;
+int height = 25;
 
-int ball_x = 39;
+int rocket_r = 6;
+int rocket_l = 17;
+
+int vector_x = 1;
+int vector_y = 1;
+int sc1 = 0;
+int sc2 = 0;
 int ball_y = 12;
-int sc1 = 0, sc2 = 0;
-int vector_x = -1;
-int vector_y = -1;
+int ball_x = 40;
 
-void ShowTable();
-void MoveRacket();
-void MoveBall();
+void pole(int, int);
+void clearScreen(void);
 void Score();
+int coordinate_x(int, int);
+int coordinate_y(int);
 int CheckScore();
 int main() {
     while (1) {
-        ShowTable();
-        MoveRacket();
-        MoveBall();
+        pole(ball_x, ball_y);
+
+        ball_x = coordinate_x(ball_x, ball_y);
         Score();
-        printf("\e[1;1H\e[2J");
-        if (CheckScore() == 1)
-            break;
-    }
-    return 0;
-}
 
-void MoveRacket() {
-    char a;
-        switch (a = getchar()) {
-            case ('a') : if ((Racketka_1 + 2) != 24) {Racketka_1+=1;} break;
-            case ('z') : if ((Racketka_1 - 2) != 0) {Racketka_1-=1;} break;
+        ball_y = coordinate_y(ball_y);
+        char c;
+        c = getchar();
+        switch (c) {
+            case ('z'):
+                if ((rocket_l + 2) != 24) {
+                    rocket_l += 1;
+                }
+                break;
+            case ('a'):
+                if ((rocket_l - 2) != 0) {
+                    rocket_l -= 1;
+                }
+                break;
 
-            case ('k') : if ((Racketka_2 + 2) != 24) {Racketka_2+=1;} break;
-            case ('m') : if ((Racketka_2 - 2) != 0) {Racketka_2-=1;} break;
+            case ('m'):
+                if ((rocket_r + 2) != 24) {
+                    rocket_r += 1;
+                }
+                break;
+            case ('k'):
+                if ((rocket_r - 2) != 0) {
+                    rocket_r -= 1;
+                }
+                break;
         }
+        clearScreen();
+        if (CheckScore() == 1) break;
+    }
 }
 void Score() {
-    if(ball_x == 1) {
+    if (ball_x == 1) {
         sc2++;
         ball_x = 39;
         ball_y = 12;
     }
-    if(ball_x == 79) {
+    if (ball_x == 79) {
         sc1++;
         ball_x = 39;
         ball_y = 12;
     }
-    
 }
-int CheckScore () {
-    if(sc1 == 3) {
-        printf("FIRST PLAYER IS WIN!!!!!! SCORE : %d - %d\n",sc1,sc2);
+
+int coordinate_x(int ball_x, int ball_y) {
+    if (ball_x == 75 && (rocket_r == ball_y - 1 || rocket_r == ball_y || rocket_r == ball_y + 1))
+        vector_x = -1;
+    if (ball_x == 4 && (rocket_r == ball_y - 1 || rocket_l == ball_y || rocket_l == ball_y + 1)) vector_x = 1;
+
+    ball_x = ball_x + vector_x;
+    return ball_x;
+}
+
+int coordinate_y(int ball_y) {
+    if (ball_y == 23) vector_y = -1;
+    if (ball_y == 1) vector_y = 1;
+    ball_y = ball_y + vector_y;
+    return ball_y;
+}
+
+int CheckScore() {
+    if (sc1 == 3) {
+        printf("FIRST PLAYER IS WIN!!!!!! SCORE : %d - %d\n", sc1, sc2);
         return 1;
     }
-    if(sc2 == 3) {
-        printf("SECOND PLAYER IS WIN!!!!!! SCORE : %d - %d\n",sc2,sc1);
+    if (sc2 == 3) {
+        printf("SECOND PLAYER IS WIN!!!!!! SCORE : %d - %d\n", sc2, sc1);
         return 1;
     }
     return 0;
 }
-void MoveBall() {
-    if (ball_y == 1 || ball_y == 23) {
-                vector_y *=(-1);
+
+void pole(int ball_x, int ball_y) {
+    for (int y = 0; y != height; y++) {
+        for (int x = 0; x != width; x++) {
+            if (y == ball_y && x == ball_x)
+                printf("o");
+            else {
+                if ((y == 1) && (x == 38)) {
+                    printf("%d||%d", sc1, sc2);
+                    x = x + 4;
+                }
+
+                if ((x == 76) && (y == rocket_r - 1 || y == rocket_r || y == rocket_r + 1) ||
+                    (x == 3) && (y == rocket_l - 1 || y == rocket_l || y == rocket_l + 1)) {
+                    printf("|");
+                } else {
+                    if ((y <= 0) || (y >= height - 1))
+                        printf("x");
+                    else {
+                        if ((x >= width - 1) || (x <= 0))
+                            printf("x");
+                        else
+                            printf(" ");
+                    }
+                }
             }
-            if (ball_x == 5 && (ball_y == Racketka_1 || ball_y == (Racketka_1 + 1) || ball_y == (Racketka_1 - 1))) {
-                vector_x *=(-1);
-            }
-            if (ball_x == 75 && (ball_y == Racketka_2 || ball_y == (Racketka_2 + 1) || ball_y == (Racketka_2 - 1))) {
-                vector_x *=(-1);
-            }
-            ball_y += vector_y;
-            ball_x += vector_x;
+        }
+
+        printf("\n");
+    }
 }
 
-void ShowTable() {
-    int i;
-    int x = 79, y = 24;
-    do {
-        for (int i = 0; x >= i; i++) {
-            if ((y == 24) || (y == 0)) {
-                printf("#");
-            } else {
-                if ((y == 23) && (i == 38)) {
-                    printf("%d||%d", sc1, sc2);
-                    i = i + 3;
-                } else {
-                    if (((i == 0) || (i == 79)) && (y != 0) && (y != 24)) {
-                        printf("#");
-                        i = i + 2;
-                    }                   
-                    if ((i == 4) && ((y == Racketka_1) || (y == Racketka_1 + 1) || (y == Racketka_1 - 1))) {
-                        printf("@");
-                        i++;
-                    }
-                    if ((i == 76) && ((y == Racketka_2) || (y == Racketka_2 + 1) || (y == Racketka_2 - 1))) {
-                        printf("@");
-                        i++;
-                    }
-                    if (i == ball_x && y == ball_y) {
-                       printf("o");
-                       i++;
-                    }
-                    printf(" ");
-                }
-                
-            }
-        
-        }
-        y--;
-        printf("\n");
-    } while (y >= 0);
-}
+void clearScreen(void) { printf("\33[0d\33[2J"); }
